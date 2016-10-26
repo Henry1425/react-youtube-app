aa4fc70271098578fc5b5ac78ddd45d77a885186
Henry Acosta <you@example.com>
2016-10-26 15:39:43 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
+const API_KEY = 'AIzaSyDiYMBpNavkiOHG12clsswm98NiAbwMIcM';
 
-import App from './components/app';
-import reducers from './reducers';
+//Create a new component. This should produce
+//some HTML.
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });//this.setState({videos:videos});
+    });
+
+  }
+  render() {
+  return (
+    <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList // Property onVideoSelect takes a func => selectedVideo func updateds the apps state.
+         onVideoSelect={selectedVideo => this.setState({selectedVideo})}// selectedVideo is a callback
+         videos={this.state.videos} />
+    </div>
+  );
+ }
+}
+
+ReactDOM.render(<App /> , document.querySelector('.container'));
+
+
+
+//Take this component's generated HTML and put it
+//on the page (in the DOM)
